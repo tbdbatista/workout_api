b94889c75fc2d34ad53b03ac4ae27d1e032b4f4b
Thiago Beltrami Dias Batista <tbdbatista@gmail.com>
2024-05-01 11:25:24 -0300

    create categoria schema

--- workout_api/categorias/schemas.py
+++ workout_api/categorias/schemas.py
@@ -1,13 +1,10 @@
 from typing import Annotated
 
-from pydantic import UUID4, Field
+from pydantic import Field
 from workout_api.contrib.schemas import BaseSchema
 
 
-class CategoriaIn(BaseSchema):
+class Categoria(BaseSchema):
     nome: Annotated[str, Field(description='Nome da categoria', example='Scale', max_length=10)]
 
-
-class CategoriaOut(CategoriaIn):
-    id: Annotated[UUID4, Field(description='Identificador da categoria')]
 
